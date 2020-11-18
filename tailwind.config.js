38e5bfd3c9f5238adfb8349c9afe7adfd31a88e8
David Daza <03dazal@gmail.com>
2020-11-18 11:26:40 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        mantis: '#78CB48',
+        'dartmouth-green': '#00733C',
+        'maize-crayola': '#F8CD46',
+        'caribbean-green': '#33CCA6',
+      },
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+        pt: ['PTSans', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
